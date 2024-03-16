8ccca99ba70a88aedcb91ee85c831e188b73c232
Sri Naga Satish Vatturu <vatturu.srinagasatish@gmail.com>
2024-03-16 16:16:24 +0530

    Completed the counter function

---
+++ JS/script.js
@@ -0,0 +1,78 @@
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
+const header = document.getElementById("header")
+let scrollStarted = false;
+
+btn.addEventListener("click", navToggle);
+document.addEventListener("scroll", scrollPage)
+
+function navToggle() {
+    btn.classList.toggle("open");
+    overlay.classList.toggle("overlay-show")
+    document.body.classList.toggle("stop-scrolling")
+    menu.classList.toggle("show-menu")
+}
+
+function scrollPage() {
+    const scrollPos = window.scrollY;
+    console.log(scrollPos);
+
+    if(scrollPos > 50)
+    {
+       header.style.opacity = "0"
+       btn.style.opacity = "0"
+    }
+    else if(scrollPos < 50)
+    {
+        header.style.opacity = "2"
+        btn.style.opacity = "2"
+    }
+
+    if(scrollPos > 100 && !scrollStarted)
+    {
+        countUp();
+        scrollStarted = true
+    }
+    else if(scrollPos < 100 && scrollStarted)
+    {
+        reset();
+        scrollStarted = false;
+    }
+}
+
+function countUp() {
+    counters.forEach((counter) => {
+        counter.innerText = '0';
+
+        const updateCounter = () => {
+            // Get count target
+            const target = +counter.getAttribute("data-target");
+            //  Get current counter value
+            const c = +counter.innerText;
+
+            // craete an increment
+            const increment = target/100;
+
+            // If counter is < target, add increment
+            if(c < target) 
+            {
+                // Round up and set counter value
+                counter.innerText = `${Math.ceil(c + increment)}`
+
+                setTimeout(updateCounter, 75);
+            }
+            else
+            {
+                counter.innerText = target;
+            }
+        }
+
+        updateCounter();
+    });
+}
+
+function reset() {
+    counters.forEach((counter) => (counter.innerHTML = "0"));
+}
